4de6173b6a7971583ece3dd7885a27c3f66429b6
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2025-03-19 15:10:12 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
             targets: ["DKImagePickerController"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zhangao0086/DKCamera", .branch("master")),
+        .package(url: "https://github.com/ipao/DKCamera", .branch("develop")),
         .package(url: "https://github.com/zhangao0086/DKPhotoGallery", .branch("master")),
-        .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.6.0"),
+        .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.7.4"),
     ],
     targets: [
         .target(
